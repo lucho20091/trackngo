39da13e37a4f413091175733f17e7995e5813bc3
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2026-02-07 18:25:22 -0500

    new changes

--- src/layouts/headers/MobileMenu.tsx
+++ src/layouts/headers/MobileMenu.tsx
@@ -1,20 +1,14 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import menu_data from "../../data/menu-data";
 
 export default function MobileMenu() {
   // menu toggle function
-  const [navTitle, setNavTitle] = useState<string>("");
-
   return (
     <div className="mean-bar d-none d-sm-block d-lg-none">
       <nav className="mean-nav">
         <ul>
           {menu_data.map((item, i) => (
-            <li
-              key={i}
-              className={`${item.has_dropdown ? "has-dropdownss" : ""} ${navTitle === item.title ? "dropdown-opened" : ""}`}
-            >
+            <li key={i}>
               <Link to={item.path}>{item.title}</Link>
             </li>
           ))}
